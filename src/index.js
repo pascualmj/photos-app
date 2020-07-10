863974dd40c3bb7a5670470aa25c9d1fd5bb1382
Manuel Pascual <manupascual90@gmail.com>
2020-07-10 16:10:12 -0300

    Initialize Redux Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider as StoreProvider } from "react-redux";
 
 import App from "./App";
-import ThemeProvider from "./context/ThemeProvider";
+import configureStore from "./store";
+
+// Theme Provider
+import ThemeProvider from "./contexts/ThemeProvider";
 
 // SCSS: Reset, Normalize & Typo
 import "./assets/scss/normalize.scss";
@@ -12,11 +16,16 @@ import "./assets/scss/typo.scss";
 // Global SCSS File
 import "./index.scss";
 
+// Initialize Redux Store
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <StoreProvider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
