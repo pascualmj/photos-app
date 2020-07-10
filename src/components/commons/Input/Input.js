4c39ffd050bea27b279f05b9bac33f0f924b4dbb
Manuel Pascual <manupascual90@gmail.com>
2020-07-10 17:29:02 -0300

    Create Input component

---
+++ src/components/commons/Input/Input.js
@@ -0,0 +1,48 @@
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import styles from "./input.module.scss";
+
+import { generateRandomString } from "../../../functions";
+
+const Input = ({
+  type,
+  value = "",
+  label = "",
+  handleChange,
+  placeholder = "",
+  className = "",
+  disabled = false,
+}) => {
+  const uniqueIdentifier = useMemo(() => generateRandomString(), []);
+
+  return (
+    <div className={className}>
+      {!!label && (
+        <label for={uniqueIdentifier} className={styles.label}>
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        id={uniqueIdentifier}
+        onChange={handleChange}
+        disabled={disabled}
+        className={styles.input}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.oneOf(["text", "email", "number", "password"]).isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  label: PropTypes.string,
+  handleChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default Input;
