e4cd8e62f084ca0adf1ccdf05a6dc03ffaf4a95f
Manuel Pascual <manupascual90@gmail.com>
2020-07-10 16:10:29 -0300

    Clean file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
 
-import Text from "./components/commons/Text";
-
-import { useTheme } from "./hooks";
-
 const App = () => {
-  const { themeMode, toggleThemeMode } = useTheme();
-
-  return (
-    <div className={`theme-${themeMode}`}>
-      <Text color="primary" size="big" weight="bold">
-        Hello from App.js!
-      </Text>
-      <button onClick={toggleThemeMode}>Toggle theme!</button>
-    </div>
-  );
+  return <div>Hello from App.js!</div>;
 };
 
 export default App;
