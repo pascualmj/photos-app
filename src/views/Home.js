c374568da0fb5c2cc28f40a7b51b765010ac9a81
Manuel Pascual <manupascual90@gmail.com>
2020-07-11 18:30:05 -0300

    Add LayoutDefault to Home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import LayoutDefault from "../layouts/LayoutDefault";
+
 const Home = () => {
-  return <div>Vista Home</div>;
+  return (
+    <LayoutDefault>
+      <div>Hola desde Home!</div>
+    </LayoutDefault>
+  );
 };
 
 export default Home;
