45611485564e419c10539ae81ebabc77194865df
Manuel Pascual <manupascual90@gmail.com>
2020-07-11 12:58:21 -0300

    Modify auth store slice

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,14 +3,21 @@ import { createSlice } from "@reduxjs/toolkit";
 const slice = createSlice({
   name: "auth",
   initialState: {
-    isAuthenticated: false,
+    isAuthenticated: JSON.parse(localStorage.getItem("userData"))
+      ? true
+      : false,
+    userData: JSON.parse(localStorage.getItem("userData")) || {},
   },
   reducers: {
     loggedIn: (state, action) => {
       state.isAuthenticated = true;
+      state.userData = action.payload;
+      localStorage.setItem("userData", JSON.stringify(action.payload));
     },
     loggedOut: (state, action) => {
       state.isAuthenticated = false;
+      state.userData = {};
+      localStorage.removeItem("userData");
     },
   },
 });
