8163476906386848a81eda49c7d7fdd533e8f26a
Manuel Pascual <manupascual90@gmail.com>
2020-07-12 12:30:56 -0300

    Remove getPhotoById selector from hook

--- src/hooks/useGlobalStore.js
+++ src/hooks/useGlobalStore.js
@@ -1,14 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import { getPhotoById } from "../store/photos";
-
 const useGlobalStore = () => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const userData = useSelector((state) => state.auth.userData);
   const photos = useSelector((state) => state.entities.photos.list);
   const photo = useSelector((state) => state.entities.photos.details);
-  const photoById = useSelector(getPhotoById);
 
   return {
     dispatch,
@@ -16,7 +13,6 @@ const useGlobalStore = () => {
     userData,
     photos,
     photo,
-    photoById,
   };
 };
 
